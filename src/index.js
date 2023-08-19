891416f3e78be777df6c930e7c1d654a2ee19a3a
Erick Pinzon <erickpinzonhuerta@gmail.com>
2023-08-19 09:50:36 -0600

    add front

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Express from "express";
-
+import { router as pages } from '../routes/pages.js'
+import { router as session } from '../routes/session.js'
+ 
 const app = Express();
 const PORT = 3000;
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
+
+app.use('/pages', pages); 
+app.use('/session', session);
+app.set('view engine', 'ejs');
